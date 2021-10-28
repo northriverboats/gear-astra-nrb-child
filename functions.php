dcb11a92ef376520f3a2a73440cc46fcb87aebb6
Sara Lynn <saral@northriverboats.com>
2021-10-28 10:45:28 -0700

    block api call to /wp/v2/users/

--- functions.php
+++ functions.php
@@ -3,4 +3,16 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+function disable_rest_endpoints ( $endpoints ) {
+    if ( isset( $endpoints['/wp/v2/users'] ) ) {
+        unset( $endpoints['/wp/v2/users'] );
+    }
+    if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+        unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+    }
+    return $endpoints;
+}
+add_filter( 'rest_endpoints', 'disable_rest_endpoints');
+
 ?>
